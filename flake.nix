b4a1489a4d53f5f78278903482d95ee181696163
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-04 10:54:32 +1300

    Minimal OpenUSD build for Nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C++ Crosscompilation Example";
+
+  inputs = { nixpkgs.url = "path:/Users/ashley/nixpkgs"; };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, pkgs, system, ... }: {
+        packages = {
+          babble = pkgs.callPackage ./package.nix { };
+          default = config.packages.babble;
+
+          x86 = pkgs.pkgsCross.gnu64.callPackage ./package.nix { };
+          x86-static =
+            pkgs.pkgsCross.gnu64.pkgsStatic.callPackage ./package.nix { };
+          aarch64 =
+            pkgs.pkgsCross.aarch64-multiplatform.callPackage ./package.nix { };
+          aarch64-static =
+            pkgs.pkgsCross.aarch64-multiplatform.pkgsStatic.callPackage
+            ./package.nix { };
+
+          windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
+        };
+      };
+      flake = { };
+    };
+}
---
+++ package.nix
@@ -0,0 +1,22 @@
+{ stdenv, lib, cmake, boost, tbb, libGL, opensubdiv, darwin }:
+stdenv.mkDerivation {
+  name = "openusd-minimal";
+
+  # good source filtering is important for caching of builds.
+  # It's easier when subprojects have their own distinct subfolders.
+  src = fetchGit {
+    url = "https://github.com/pixaranimationstudios/openusd";
+    ref = "refs/tags/v23.11";
+    rev = "0b18ad3f840c24eb25e16b795a5b0821cf05126e";
+  };
+
+  # Distinguishing between native build inputs (runnable on the host
+  # at compile time) and normal build inputs (runnable on target
+  # platform at run time) is important for cross compilation.
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ boost tbb libGL opensubdiv ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [Cocoa Metal MetalKit]);
+
+  cmakeFlags = [
+    "-DPXR_ENABLE_PYTHON_SUPPORT=false"
+  ];
+}
