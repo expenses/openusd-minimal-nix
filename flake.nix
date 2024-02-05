14949a03a446d9ee5d0cbe5ebae0127262433326
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-05 23:45:10 +1300

    Simplify options for mac

--- flake.nix
+++ flake.nix
@@ -8,16 +8,19 @@
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, pkgs, system, ... }: {
-        packages = {
-          default = pkgs.callPackage ./package.nix { };
+        packages = let
+          # Override the stdenv for darwin as we need to use the 11.0 sdk and not 10.x
+          stdenv = if pkgs.stdenv.isDarwin then
+            pkgs.darwin.apple_sdk_11_0.stdenv
+          else
+            pkgs.stdenv;
+        in {
+          default = pkgs.callPackage ./package.nix { stdenv = stdenv; };
 
-          static = pkgs.callPackage ./package.nix { static = true; };
-
-          apple-sdk-11 =
-            pkgs.darwin.apple_sdk_11_0.callPackage ./package.nix { };
-
-          apple-sdk-11-static =
-            pkgs.darwin.apple_sdk_11_0.callPackage ./package.nix { static = true; };
+          static = pkgs.callPackage ./package.nix {
+            stdenv = stdenv;
+            static = true;
+          };
 
           windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
         };
