aeeec7de58964bc138f10f80f7266866fc6ac44a
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-08 14:17:42 +1300

    Make OIIO support optional, add materialx support

--- flake.nix
+++ flake.nix
@@ -1,28 +1,39 @@
 {
   description = "C++ Crosscompilation Example";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    materialx.url = "github:expenses/materialx-nix";
+    materialx.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, materialx, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, pkgs, system, ... }: {
         packages = let
-          # Override the stdenv for darwin as we need to use the 11.0 sdk and not 10.x
-          stdenv = if pkgs.stdenv.isDarwin then
-            pkgs.darwin.apple_sdk_11_0.stdenv
-          else
-            pkgs.stdenv;
-        in {
-          default = pkgs.callPackage ./package.nix { stdenv = stdenv; };
-
-          static = pkgs.callPackage ./package.nix {
-            stdenv = stdenv;
-            static = true;
+          args = {
+            # Override the stdenv for darwin as we need to use the 11.0 sdk and not 10.x
+            stdenv = if pkgs.stdenv.isDarwin then
+              pkgs.darwin.apple_sdk_11_0.stdenv
+            else
+              pkgs.stdenv;
+            materialx = materialx.packages.${system}.default;
           };
+          fullArgs = args // {
+            embreeSupport = true;
+            dracoSupport = true;
+            openimageioSupport = true;
+            materialxSupport = true;
+          };
+        in {
+          default = pkgs.callPackage ./package.nix args;
+          full = pkgs.callPackage ./package.nix fullArgs;
 
-          windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix { };
+          static = pkgs.callPackage ./package.nix (args // { static = true; });
+
+          windows = pkgs.pkgsCross.mingwW64.callPackage ./package.nix args;
         };
       };
       flake = { };
--- package.nix
+++ package.nix
@@ -1,5 +1,6 @@
-{ stdenv, lib, cmake, boost, tbb, libGL, opensubdiv, darwin, xorg, embree, draco, openimageio, openexr, imath
-, static ? false, embreeSupport ? false, dracoSupport ? false }:
+{ stdenv, lib, cmake, boost, tbb, libGL, opensubdiv, darwin, xorg, embree, draco
+, openimageio, openexr, imath, materialx, static ? false, embreeSupport ? false
+, dracoSupport ? false, openimageioSupport ? false, materialxSupport ? false }:
 stdenv.mkDerivation {
   name = "openusd-minimal";
 
@@ -16,15 +17,21 @@ stdenv.mkDerivation {
   # platform at run time) is important for cross compilation.
   nativeBuildInputs = [ cmake ];
 
-  buildInputs = [ boost tbb libGL opensubdiv openimageio openexr imath ]
+  buildInputs = [ boost tbb libGL opensubdiv ]
     ++ lib.optionals stdenv.isLinux ([ xorg.libX11 ])
     ++ lib.optionals stdenv.isDarwin
     (with darwin.apple_sdk_11_0.frameworks; [ Cocoa MetalKit ])
     ++ lib.optionals embreeSupport ([ embree ])
-    ++ lib.optionals dracoSupport ([ draco ]);
+    ++ lib.optionals dracoSupport ([ draco ])
+    ++ lib.optionals openimageioSupport ([ openimageio openexr imath ])
+    ++ lib.optionals materialxSupport ([ materialx ]);
 
-  cmakeFlags = [ "-DPXR_ENABLE_PYTHON_SUPPORT=false" "-DPXR_BUILD_OPENIMAGEIO_PLUGIN=true" ]
+  cmakeFlags = [ "-DPXR_ENABLE_PYTHON_SUPPORT=false" ]
     ++ lib.optionals embreeSupport ([ "-DPXR_BUILD_EMBREE_PLUGIN=true" ])
     ++ lib.optionals dracoSupport ([ "-DPXR_BUILD_DRACO_PLUGIN=true" ])
+    ++ lib.optionals openimageioSupport
+    ([ "-DPXR_BUILD_OPENIMAGEIO_PLUGIN=true" ])
+    ++ lib.optionals materialxSupport
+    ([ "-DPXR_ENABLE_MATERIALX_SUPPORT=true" ])
     ++ lib.optionals static ([ "-DBUILD_SHARED_LIBS=false" ]);
 }
